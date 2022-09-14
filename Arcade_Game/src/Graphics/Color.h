eab0cebb40e39b063abd5ea321ec202ad57cb028
wildpaco <danielcrespo63@gmail.com>
2022-09-15 00:27:12 +0200

    color class, have to resolve alpha pixel format issue

---
+++ Arcade_Game/src/Graphics/Color.h
@@ -0,0 +1,65 @@
+//=================================================================================
+// Namespace		: Arcade_Game
+// Name				: Color.h
+// Author			: Daniel Crespo Muñoz
+// Version			:
+// Copyright		:
+// Description		:
+// Creation time	: 14/09/2022 23:49:55
+//=================================================================================
+
+#ifndef _Arcade_Game_Color_H_
+#define _Arcade_Game_Color_H_
+
+#pragma once
+
+#include <stdint.h>
+
+struct SDL_PixelFormat;
+
+class Color
+{
+private:
+	uint32_t mColor;
+
+protected:
+
+public:
+
+	static const SDL_PixelFormat* mFormat;
+	static void InitColorFormat(const SDL_PixelFormat* format);
+
+	static Color Black() { return Color(0, 0, 0, 255); }
+	static Color White() { return Color(255, 255, 255, 255); }
+	static Color Blue() { return Color(0, 0, 255, 255); }
+	static Color Red() { return Color(255, 0, 0, 255); }
+	static Color Green() { return Color(0, 255, 0, 255); }
+	static Color Yellow() { return Color(255, 255, 0, 255); }
+	static Color Magenta() { return Color(255, 0, 255, 255); }
+	static Color Cyan() { return Color(37, 240, 217, 255); }
+	static Color Pink() { return Color(252, 197, 224, 255); }
+	static Color Orange() { return Color(245, 190, 100, 255); }
+
+
+	Color();
+	Color(uint32_t color);
+	Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a);
+
+	inline bool operator==(const Color& c) const { return mColor == c.mColor; }
+	inline bool operator!=(const Color& c) const { return !(*this == c); }
+	inline uint32_t GetPixelColor() const { return mColor; }
+
+	void SetRGBA(uint8_t r, uint8_t g, uint8_t b, uint8_t a);
+
+	void SetRed(uint8_t red);
+	void SetGren(uint8_t green);
+	void SetBlue(uint8_t blue);
+	void SetAlpha(uint8_t alpha);
+
+	uint8_t GetRed() const;
+	uint8_t GetGreen() const;
+	uint8_t GetBlue() const;
+	uint8_t GetAlpha() const;
+};
+#endif // !_Arcade_Game_Color_H_
+
--- Arcade_Game/src/main.cpp
+++ Arcade_Game/src/main.cpp
@@ -1,17 +1,101 @@
+//=================================================================================
+// Namespace		: Arcade_Game
+// Name				: main.cpp
+// Author			: Daniel Crespo Muñoz
+// Version			:
+// Copyright		:
+// Description		:
+// Creation time	: 14/09/2022 20:41:00
+//=================================================================================
+
+
 #include <iostream>
 #include <SDL.h>
 #include "Vec2D.h"
-#include "DynamicIntArray.h"
+#include "Color.h"
+
+const int SCREEN_WIDTH = 250;
+const int SCREN_HEIGHT = 250;
+
+void SetPixel(SDL_Surface* noptrWindowSurface, uint32_t color, int x, int y);
+size_t GetIndex(SDL_Surface* noptrSurface, int r, int c);
 
 int main(int arg, char *argv[])
 {	
-	Vec2D gordo(32,45);
-	Vec2D cipote;
 
-	cipote = 4 * gordo;
+	if (SDL_Init(SDL_INIT_VIDEO))
+	{
+		std::cout << "Error SDL_Init Failed" << std::endl;
+		return 1;
+	}
 
-	std::cout << -gordo;
-	std::cout << cipote;
+	SDL_Window* optrWindow = SDL_CreateWindow("Arcade", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREN_HEIGHT, 0);
+
+	if (optrWindow == nullptr)
+	{
+		std::cout << "Could not create window, got error: " << SDL_GetError() << std::endl;
+	}
+
+	SDL_Surface* noptrWindowSurface = SDL_GetWindowSurface(optrWindow);
 	
+
+	SDL_PixelFormat* pixelFormat = SDL_AllocFormat(SDL_PIXELFORMAT_RGBA8888);
+	
+	Color::InitColorFormat(pixelFormat);
+	
+
+	std::cout << "The window pixel format is: " << SDL_GetPixelFormatName(pixelFormat->format);
+
+
+	uint32_t color = 0xFF0000;
+
+	for (size_t i = 0; i < 100; i++)
+	{
+		SetPixel(noptrWindowSurface, Color::Green().GetPixelColor(), i, i);
+	}
+
+	//SetPixel(noptrWindowSurface, Color::Green().GetPixelColor(), SCREEN_WIDTH / 2, SCREN_HEIGHT / 2);
+	
+	SDL_UpdateWindowSurface(optrWindow);
+
+	SDL_Event sdlEvent;
+	bool running = true;
+
+	while (running)
+	{
+		while (SDL_PollEvent(&sdlEvent))
+		{
+			switch (sdlEvent.type)
+			{
+			case SDL_QUIT:
+				running = false;
+				break;
+			}
+		}
+	}
+
+
+
+	SDL_DestroyWindow(optrWindow);
+	SDL_Quit();
+
 	return 0;
+}
+
+void SetPixel(SDL_Surface* noptrWindowSurface, uint32_t color, int x, int y)
+{
+	SDL_LockSurface(noptrWindowSurface);
+
+	uint32_t* pixels = (uint32_t*)noptrWindowSurface->pixels;
+
+	size_t index = GetIndex(noptrWindowSurface, y, x);
+
+	pixels[index] = color;
+
+	SDL_UnlockSurface(noptrWindowSurface);
+}
+
+size_t GetIndex(SDL_Surface* noptrSurface, int r, int c)
+{
+	return r * noptrSurface->w + c;
 }
