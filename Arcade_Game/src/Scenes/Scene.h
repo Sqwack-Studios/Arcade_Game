08325798379943407345d0b40e22aa8bb21755b5
wildpaco <danielcrespo63@gmail.com>
2022-09-21 20:33:37 +0200

    Scene class interface

---
+++ Arcade_Game/src/Scenes/Scene.h
@@ -0,0 +1,34 @@
+//=================================================================================
+// Namespace		: Arcade_Game
+// Name				: Scene.h
+// Author			: Daniel Crespo Muñoz
+// Version			:
+// Copyright		:
+// Description		:
+// Creation time	: 21/09/2022 20:28:41
+//=================================================================================
+
+#ifndef _Arcade_Game_Scene_H_
+#define _Arcade_Game_Scene_H_
+
+#pragma once
+#include <string>
+#include <stdint.h>
+class Screen;
+
+//Interface Scene Class
+class Scene
+{
+private:
+
+protected:
+
+public:
+	virtual ~Scene() {}
+	virtual void Init() = 0;
+	virtual void Update(uint32_t deltaTime) = 0;
+	virtual void Draw(Screen& theScreen) = 0;
+	virtual const std::string& GetSceneName() const = 0;
+};
+#endif // !_Arcade_Game_Scene_H_
+
